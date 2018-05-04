bd324108d0175d4d32601af31f59dd1bfe8e8e1e
Thomas Szewczyk <thomas.szewczyk@inl.gov>
2018-05-04 10:56:09 -0600

    fixing zoom issue

--- Assets/Scripts/zoom.cs
+++ Assets/Scripts/zoom.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class zoom : MonoBehaviour {
 
 	public float scaleSens = 0.1f;
+	public GameObject menu;
+
 
 	// Use this for initialization
 	void Start () {
@@ -13,12 +15,15 @@ public class zoom : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetAxis ("Mouse ScrollWheel") > 0) { 
-			transform.localScale = new Vector3 (transform.localScale.x + scaleSens, transform.localScale.y + scaleSens, transform.localScale.z + scaleSens);
-		}
 
-		if (Input.GetAxis ("Mouse ScrollWheel") < 0) {
-			transform.localScale = new Vector3 (transform.localScale.x - scaleSens, transform.localScale.y - scaleSens, transform.localScale.z - scaleSens);
+		if (!menu.activeSelf) {
+			if (Input.GetAxis ("Mouse ScrollWheel") > 0) { 
+				transform.localScale = new Vector3 (transform.localScale.x + scaleSens, transform.localScale.y + scaleSens, transform.localScale.z + scaleSens);
+			}
+
+			if (Input.GetAxis ("Mouse ScrollWheel") < 0) {
+				transform.localScale = new Vector3 (transform.localScale.x - scaleSens, transform.localScale.y - scaleSens, transform.localScale.z - scaleSens);
+			}
 		}
 	}
 }
