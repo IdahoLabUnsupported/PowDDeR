c3b42ac228f179adeda4d434bb9ac821bbf53786
Thomas Szewczyk <thomas.szewczyk@inl.gov>
2018-05-22 16:11:12 -0600

    adding in negative mesh load manifold, now when things go in the opposite direction, a new manifold will be generated in red to indicate a power deficit

--- Assets/Scripts/GenerateMesh.cs
+++ Assets/Scripts/GenerateMesh.cs
@@ -20,6 +20,7 @@ public class GenerateMesh : MonoBehaviour {
 	static int MAX_MESHES = 50;
 
 	List<Vector3>[] points = new List<Vector3>[MAX_MESHES];
+	List<Vector3>[] negativePoints = new List<Vector3>[MAX_MESHES];
 	Vector3 mins = Vector3.zero;
 	Vector3 maxs = Vector3.zero;
 
@@ -32,7 +33,12 @@ public class GenerateMesh : MonoBehaviour {
 	List<GameObject> displayCubeList = new List<GameObject>();
 	public Transform displayCubeHolder;
 
-	private Mesh[] meshBack = new Mesh[MAX_MESHES];
+
+	public GameObject negativeDisplayCube;
+	List<GameObject> negativeDisplayCubeList = new List<GameObject>();
+	public Transform negativeDisplayCubeHolder;
+
+	private Mesh[] meshBack = new Mesh[MAX_MESHES*2];
 
 	VectorLine line;
 	VectorLine line2d;
@@ -46,6 +52,8 @@ public class GenerateMesh : MonoBehaviour {
 		for (int i = 0; i < MAX_MESHES; i++) {
 			points [i] = new List<Vector3> ();
 			points [i].Capacity = MAX_VERTEX;
+			negativePoints [i] = new List<Vector3> ();
+			negativePoints [i].Capacity = MAX_VERTEX;
 			meshBack[i] = new Mesh();
 		}
 
@@ -121,6 +129,7 @@ public class GenerateMesh : MonoBehaviour {
 
 		int count = 0;
 		int numMesh = 0;
+		int numNegativeMesh = 0;
 		bool runOnce = false;
 
 		int index;
@@ -171,34 +180,99 @@ public class GenerateMesh : MonoBehaviour {
 					for (int i = 0; i < vertices.Count; i++) {
 
 						Vector3 point = vertices [i];
-
+						Vector3 negativePoint = vertices [i];
 						numMesh = count / MAX_VERTEX;
 
-						// if this is the first asset just add it to the points because no addition needs to be calculated
+						// if this is the first asset just add it appropriately to the points because no addition needs to be calculated
 						if (first && !asset.load) {
 							points [numMesh].Add (point);
 
-						} else {
+						} else if (first && asset.load) {
+							negativePoints [numMesh].Add (point);
+
+						}else{
 							float newX = 0.0f;
 							float newY = 0.0f;
+
+							float negativeX = 0.0f;
+							float negativeY = 0.0f;
 
 							if (index >= MAX_VERTEX) {
 								index = 0;
 							}
-							// if this isn't a load add in the points, otherwise subtract it
+							// if this isn't a load shrink the negative manifold and grow the positive manifold
 							if (!asset.load) {
 
-								newX = points [numMesh] [index].x + point.x;
-								newY = points [numMesh] [index].y + point.y;
-							} else if(points [numMesh].Count>0){
 
-								if (points [numMesh] [index].x - point.x > 0) {
-									newX = points [numMesh] [index].x - point.x;
-								} 
+								//if we are still at negative side of manifold and the addition of this point doesn't change the sign of this 
+								//point, shrink the negative manifold and set the positive manifold zero
+								if (negativePoints [numMesh] [index].x != 0.0f && Mathf.Sign (negativePoints [numMesh] [index].x - point.x) == Mathf.Sign (negativePoints [numMesh] [index].x)) {
+									negativeX = negativePoints [numMesh] [index].x - point.x;
+									newX = 0.0f;
+									//else if there is a sign change
+								} else if (negativePoints [numMesh] [index].x != 0.0f && Mathf.Sign (negativePoints [numMesh] [index].x - point.x) != Mathf.Sign (negativePoints [numMesh] [index].x)) {
+									//add the difference to the manifold and zeroize this negative manifold point
+									newX = points [numMesh] [index].x + negativePoints [numMesh] [index].x - point.x;
+									negativeX = 0.0f;
 
-								if (points [numMesh] [index].y - point.y > 0) {
-									newY = points [numMesh] [index].y - point.y;
+									//else theres no negative component to worry about, just add everything together
+								} else {
+									negativeX = 0.0f;
+									newX = points [numMesh] [index].x + point.x;
 								}
+
+
+								if (negativePoints [numMesh] [index].y != 0.0f && Mathf.Sign (negativePoints [numMesh] [index].y - point.y) == Mathf.Sign (negativePoints [numMesh] [index].y)) {
+									negativeY = negativePoints [numMesh] [index].y - point.y;
+									newY = 0.0f;
+								//else if there is a sign change
+								} else if (negativePoints [numMesh] [index].y != 0.0f && Mathf.Sign (negativePoints [numMesh] [index].y - point.y) != Mathf.Sign (negativePoints [numMesh] [index].y)) {
+									//add the difference to the manifold and zeroize this negative manifold point
+									negativeY = 0.0f;
+									newY = points [numMesh] [index].y + negativePoints [numMesh] [index].y - point.y;
+								//else theres no negative component to worry about, just add everything together
+								} else {
+									negativeY = 0.0f;
+									newY = points [numMesh] [index].y + point.y;
+								}
+
+
+								// this is a load that should shrink positive manifold and grow negative manifold
+							} else {
+
+								//if we are still at positive side of manifold and the subtraction of this point doesn't change the sign of this 
+								//point, shrink the positive manifold and set the negative manifold zero
+								if (points [numMesh] [index].x != 0.0f && Mathf.Sign (points [numMesh] [index].x - point.x) == Mathf.Sign (points [numMesh] [index].x)) {
+									negativeX = 0.0f;
+									newX = points [numMesh] [index].x - point.x;
+									//else if there is a sign change
+								} else if (points [numMesh] [index].x != 0.0f && Mathf.Sign (points [numMesh] [index].x - point.x) != Mathf.Sign (points [numMesh] [index].x)) {
+									//add the difference to the negative manifold and zeroize this positive manifold point
+									negativeX = negativePoints [numMesh] [index].x + points [numMesh] [index].x - point.x;
+									newX = 0.0f;
+
+									//else theres no positive component to worry about, just add everything together
+								} else {
+									newX = 0.0f;
+									negativeX = negativePoints [numMesh] [index].x + point.x;
+								}
+
+
+								if (points [numMesh] [index].y != 0.0f && Mathf.Sign (points [numMesh] [index].y - point.y) == Mathf.Sign (points [numMesh] [index].y)) {
+									negativeY = 0.0f;
+									newY = points [numMesh] [index].y - point.y;
+									//else if there is a sign change
+								} else if (points [numMesh] [index].y != 0.0f && Mathf.Sign (points [numMesh] [index].y - point.y) != Mathf.Sign (points [numMesh] [index].y)) {
+									//add the difference to the negative manifold and zeroize this positive manifold point
+									negativeY = negativePoints [numMesh] [index].y + points [numMesh] [index].y - point.y;
+									newY = 0.0f;
+
+									//else theres no positive component to worry about, just add everything together
+								} else {
+									newY = 0.0f;
+									negativeY = negativePoints [numMesh] [index].y + point.y;
+								}
+
 
 							}
 								
@@ -206,9 +280,13 @@ public class GenerateMesh : MonoBehaviour {
 							float newZ = point.z;
 
 							point = new Vector3 (newX, newY, newZ);
-
+							negativePoint = new Vector3 (negativeX, negativeY, newZ);
 							if (points [numMesh].Count > 0) {
 								((List<Vector3>)(points [numMesh])) [index] = point;
+							}
+
+							if (negativePoints [numMesh].Count > 0) {
+								((List<Vector3>)(negativePoints [numMesh])) [index] = negativePoint;
 							}
 
 							index++;
@@ -232,6 +310,16 @@ public class GenerateMesh : MonoBehaviour {
 						if (point.z < mins.z) {
 							mins = new Vector3 (mins.x, mins.y, point.z);
 						}
+						if (negativePoint.x < mins.x) {
+							mins = new Vector3 (negativePoint.x, mins.y, mins.z);
+						}
+						if (negativePoint.y < mins.y) {
+							mins = new Vector3 (mins.x, negativePoint.y, mins.z);
+						}
+						if (negativePoint.z < mins.z) {
+							mins = new Vector3 (mins.x, mins.y, negativePoint.z);
+						}
+
 
 						//figure out the maxs
 						if (point.x > maxs.x) {
@@ -242,6 +330,15 @@ public class GenerateMesh : MonoBehaviour {
 						}
 						if (point.z > maxs.z) {
 							maxs = new Vector3 (maxs.x, maxs.y, point.z);
+						}
+						if (negativePoint.x > maxs.x) {
+							maxs = new Vector3 (negativePoint.x, maxs.y, maxs.z);
+						}
+						if (negativePoint.y > maxs.y) {
+							maxs = new Vector3 (maxs.x, negativePoint.y, maxs.z);
+						}
+						if (negativePoint.z > maxs.z) {
+							maxs = new Vector3 (maxs.x, maxs.y, negativePoint.z);
 						}
 					}
 
@@ -262,6 +359,7 @@ public class GenerateMesh : MonoBehaviour {
 		Debug.Log ("Number of meshes needed " + totalMeshes);
 
 		createMesh ();
+		createNegativeMesh ();
 		drawLine ();
 		drawAxisLabels ();
 	}
@@ -278,7 +376,7 @@ public class GenerateMesh : MonoBehaviour {
 		int currentMesh = 0;
 
 		// generate the faces
-		while (currentMesh <= totalMeshes) {
+		while (currentMesh < totalMeshes) {
 			int numPoints = points [currentMesh].Count;
 
 			int[] indecies = new int[numPoints];
@@ -292,7 +390,8 @@ public class GenerateMesh : MonoBehaviour {
 			for (int j = 0; j < numPoints; j++) {
 
 				// replace current point with it's normalized value
-				points [currentMesh][j] = normalizePoints (points [currentMesh][j]);
+				points [currentMesh] [j] = normalizePoints (points [currentMesh] [j]);
+
 				indecies [j] = j;
 				colors [j] = Color.red;
 
@@ -332,8 +431,9 @@ public class GenerateMesh : MonoBehaviour {
 
 
 			meshBack[currentMesh] = new Mesh();
-			meshBack[currentMesh].SetVertices (points[currentMesh]);
 
+			meshBack[currentMesh].SetVertices (points[currentMesh]);
+		
 			meshBack[currentMesh].colors = colors;
 
 			// generate mesh
@@ -347,7 +447,9 @@ public class GenerateMesh : MonoBehaviour {
 			GameObject display = Instantiate (displayCube,displayCubeHolder);
 			display.GetComponent<MeshFilter> ().mesh.Clear ();
 			display.GetComponent<MeshFilter> ().mesh = meshBack [currentMesh];
+
 			display.GetComponent<Renderer>().material.SetColor ("_Color", settings.currentSettings.color);
+
 			displayCubeList.Add (display);
 
 			currentMesh++;
@@ -355,7 +457,104 @@ public class GenerateMesh : MonoBehaviour {
 
 		displayCube.GetComponent<Renderer>().enabled = false;
 	}
+	void createNegativeMesh()
+	{
+		//clear out old meshes
+		foreach (GameObject item in negativeDisplayCubeList) {
+			Destroy (item);
+		}
 
+		negativeDisplayCube.GetComponent<Renderer> ().enabled = true;
+
+		int currentMesh = 0;
+
+		// generate the faces
+		while (currentMesh <= totalMeshes) {
+			int numPoints = 0;
+
+			numPoints = negativePoints [currentMesh].Count;
+
+			int[] indecies = new int[numPoints];
+			int[] triangles = new int[Mathf.CeilToInt((numPoints/polarSteps)*(polarSteps*2)*3)];
+
+			int bandCount = 1;
+
+			// create new colors array where the colors will be created.
+			Color[] colors = new Color[numPoints];
+
+			for (int j = 0; j < numPoints; j++) {
+
+				// replace current point with it's normalized value
+				negativePoints [currentMesh] [j] = normalizePoints (negativePoints [currentMesh] [j]);
+
+				indecies [j] = j;
+				colors [j] = Color.red;
+
+				//if this is not the last row of points and not at the end of a band
+				if((j+fieldCount + 1)<numPoints && (j != (bandCount*(fieldCount) - 1)))
+				{
+
+					//front side
+					triangles [j*12/2] = j;
+					triangles [j*12/2 + 1] = j + 1;
+					triangles [j*12/2 + 2] = j + fieldCount;
+
+					triangles [j*12/2 + 3] = j + 1;
+					triangles [j*12/2 + 4] = j + 1 + fieldCount;
+					triangles [j*12/2 + 5] = j + fieldCount;
+				}
+
+				// if this is at the end of a band, close the loop and increment to the next band
+				if (j == (bandCount*(fieldCount) - 1)) {
+
+					// if this isn't the very last band
+					if ((j + fieldCount) < numPoints) {
+						//front side
+						triangles [j * 12 / 2] = j;
+						triangles [j * 12 / 2 + 1] = j - (fieldCount - 1);
+						triangles [j * 12 / 2 + 2] = j + 1;
+
+						triangles [j * 12 / 2 + 3] = j;
+						triangles [j * 12 / 2 + 4] = j + 1;
+						triangles [j * 12 / 2 + 5] = j + fieldCount - 1;
+					}
+
+					bandCount++;
+				}
+
+			}
+
+
+			meshBack[currentMesh] = new Mesh();
+
+			meshBack[currentMesh].SetVertices (negativePoints [currentMesh]);
+
+
+
+			meshBack[currentMesh].colors = colors;
+
+			// generate mesh
+			meshBack[currentMesh].triangles = triangles;
+			meshBack[currentMesh].RecalculateNormals();
+			//NormalSolver.RecalculateNormals(meshBack[i],60.0f);
+
+			//generate points
+			//meshBack[currentMesh].SetIndices (indecies, MeshTopology.Points, 0);
+
+			GameObject display = Instantiate (negativeDisplayCube,negativeDisplayCubeHolder);
+			display.GetComponent<MeshFilter> ().mesh.Clear ();
+			display.GetComponent<MeshFilter> ().mesh = meshBack [currentMesh];
+
+			display.GetComponent<Renderer>().material.SetColor ("_Color", Color.red);
+
+
+			negativeDisplayCubeList.Add (display);
+
+			currentMesh++;
+		}
+
+		negativeDisplayCube.GetComponent<Renderer>().enabled = false;
+	}
 	// draw the line
 	public void drawLine(float? stime = null)
 	{
@@ -397,6 +596,8 @@ public class GenerateMesh : MonoBehaviour {
 		//line.drawTransform = displayCube.transform;
 	}
 
+
+	//
 	public void updateObjectColor()
 	{
 		foreach(GameObject display in displayCubeList)
